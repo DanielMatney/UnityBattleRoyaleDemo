99f294624bd490789e9815509d83e3020db1f4ff
BuildTools <unconfigured@null.spigotmc.org>
2022-09-30 13:58:27 -0500

    Added basic enemy attack functionality

---
+++ Scripts/EnemyAttack.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+
+    public ParticleSystem bulletParticleSystem;
+    private ParticleSystem.EmissionModule em;
+
+    void Start()
+    {
+        em = bulletParticleSystem.emission;
+    }
+
+    private int RaycastLength = 10;
+
+    private float FiringRate = 10f;
+
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, forward, RaycastLength))
+        {
+            Attack();
+
+            em.rateOverTime = FiringRate;
+        }
+
+        else
+        {
+            em.rateOverTime = 0f;
+        }
+    }
+
+    void Attack()
+    {
+        Ray ray = new Ray(bulletParticleSystem.transform.position, bulletParticleSystem.transform.forward);
+        float rayLength = 100f;
+        float damage = 10f;
+
+        if (Physics.Raycast(ray, out RaycastHit hit, rayLength))
+        {
+            var objectHit = hit.collider.GetComponent<PlayerHealth>();
+
+            if (objectHit != null)
+            {
+                objectHit.ReduceHealth(damage);
+            }
+        }
+    }
+}
--- Scripts/PlayerHealth.cs
+++ Scripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -9,5 +10,18 @@ public class PlayerHealth : MonoBehaviour
     public void ReduceHealth(float damage)
     {
         health -= damage;
+
+        if (health < 1)
+        {
+            if (this.gameObject.tag == "Enemy")
+            {
+                Destroy(this.gameObject);
+            }
+
+            else if (this.gameObject.tag == "Player")
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
     }
 }
