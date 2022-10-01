a04ea88449cf30064ef805507685b5bd84ed3142
BuildTools <unconfigured@null.spigotmc.org>
2022-09-30 21:53:21 -0500

    Added random enemy movement

---
+++ Scripts/EnemyMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    CharacterController controller;
+    float heading;
+    public float directionChangeInterval = 1f;
+    public float maxHeadingChange = 30;
+    Vector3 targetRotation;
+    public float speed = 1f;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+
+        heading = Random.Range(0, 360);
+
+        transform.eulerAngles = new Vector3(0, heading, 0);
+
+        StartCoroutine(NewHeading());
+    }
+
+    IEnumerator NewHeading()
+    {
+        while (true)
+        {
+            NewHeadingRoutine();
+
+            yield return new WaitForSeconds(directionChangeInterval);
+        }
+    }
+
+    void NewHeadingRoutine()
+    {
+        var floor = Mathf.Clamp(heading - maxHeadingChange, 0, 360);
+
+        var ceiling = Mathf.Clamp(heading + maxHeadingChange, 0, 360);
+
+        heading = Random.Range(floor, ceiling);
+
+        targetRotation = new Vector3(0, heading, 0);
+    }
+
+    void Update()
+    {
+        transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
+
+        var forward = transform.TransformDirection(Vector3.forward);
+
+        controller.SimpleMove(forward * speed);
+    }
+}
